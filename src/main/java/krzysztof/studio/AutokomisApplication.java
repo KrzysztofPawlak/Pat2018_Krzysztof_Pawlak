6860c17e6c6187ab8bac7c6cf3bfcc427e5488a6
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2017-12-27 16:49:54 +0100

    initial commit

---
+++ src/main/java/krzysztof/studio/AutokomisApplication.java
@@ -0,0 +1,13 @@
+package krzysztof.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutokomisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutokomisApplication.class, args);
+		System.out.println("working");
+	}
+}
---
+++ src/test/java/krzysztof/studio/Pat2018KrzysztofPawlakApplicationTests.java
@@ -0,0 +1,16 @@
+package krzysztof.studio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Pat2018KrzysztofPawlakApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
